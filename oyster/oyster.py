d2cbf6f8474dfd1217aed891cf898c8ecb615fb6
Brennan Barker <BrennanBarker@users.noreply.github.com>
2020-06-12 15:31:29 -0400

    basis_set function

--- oyster/oyster.py
+++ oyster/oyster.py
@@ -1,7 +1,7 @@
 """Top-level functions for inference from causal diagrams."""
 
 import networkx as nx
-from oyster.utils.graph_utils import NA_pairs
+from oyster.utils.graph_utils import NA_pairs, De, Pa
 from oyster.utils.set_utils import minimal_sets
 from oyster.adjust import d_separated, d_separator_search
 from oyster.viz.viz import draw
@@ -13,3 +13,8 @@ def implied_independencies(DAG):
     for (X, Y), minsets in ind.items():
            print(f'{X} ⫫ {Y} | {minsets}')
 
+def basis_set(DAG):
+    for v in DAG.nodes:
+        pa = Pa(DAG, v)
+        nondesc = DAG.nodes - {v} - De(DAG, v) - pa 
+        if nondesc: print(f'{v} ⫫ {nondesc} | {pa}')
