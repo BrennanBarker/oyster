94a5db0140c5bba215f01639e252e2a12f4e1382
Brennan Barker <BrennanBarker@users.noreply.github.com>
2020-06-13 14:06:54 -0400

    sq351

--- oyster/__init__.py
+++ oyster/__init__.py
@@ -6,4 +6,5 @@ from oyster.utils.graph_utils import *
 from oyster.utils.set_utils import *
 import oyster.example.graphs as ex
 from oyster.viz.viz import *
+from oyster.viz.formulas import *
 from oyster.equivalence import dag_to_cpdag, equivalence_class_size
