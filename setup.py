b20c0b32b683b3ea521b8ad1dcf2524251b089ed
Brennan Barker <BrennanBarker@users.noreply.github.com>
2020-07-06 15:24:35 -0400

    add matplotlib and networkx requirements

--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/BrennanBarker/oyster",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'matplotlib'
+        'networkx'
+    ]
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
